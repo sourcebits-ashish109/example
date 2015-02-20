1b4e14eb11c3451fab6746488e1e123438412cdc
Ashish Prabhakar <ashish.prabhakar@sourcebits.com>
2015-02-20 17:31:07 +0530

    second commit

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exampleApp
+ * @description
+ * # exampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exampleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
